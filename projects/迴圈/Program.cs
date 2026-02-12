89303ecfe017cb1285cd3d600be9d0f3af82caab
bootsbot <bootshas@gmail.com>
2026-02-12 17:28:46 +0800

    update

---
+++ projects/迴圈/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+int i=1;
+
+while ( i <=5)
+
+{
+    for (int j=1;j<=i;)
+    {
+        
+    
+    Console.WriteLine(i);
+    i++;
+    }
+    
+}    
+Console.WriteLine("enter");
+Console.ReadLine();
