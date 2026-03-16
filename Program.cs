dd1d0a9512134e27e6e7e967112512845eb1345b
bootsbot <bootshas@gmail.com>
2026-03-16 18:22:22 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+public interface IAnimal
+{
+    void Speak();
+    void Move();
+}
+
+public class Dog : IAnimal
+{
+    public void Speak()
+    {
+        Console.WriteLine("Woof!");
+    }
+
+    public void Move()
+    {
+        Console.WriteLine("The dog runs.");
+    }
+}
+
+public class Cat : IAnimal
+{
+    public void Speak()
+    {
+        Console.WriteLine("Meow!");
+    }
+
+    public void Move()
+    {
+        Console.WriteLine("The cat walks gracefully.");
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        IAnimal dog = new Dog();
+        IAnimal cat = new Cat();
+
+        dog.Speak();
+        dog.Move();
+
+        cat.Speak();
+        cat.Move();
+    }
+}
